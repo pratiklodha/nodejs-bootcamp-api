a6f9ea314743e1b0d07de757cd28ab8440607839
Pratik Lodha <pratiklodha01@gmail.com>
2019-12-21 13:07:32 +0530

    setting mongoose and connecting mongodb

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({path: './config/config.env'});
+
+// Connect to database
+connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -23,4 +28,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+// Handle unhandeled promise rejections
+process.on('unhandeledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
